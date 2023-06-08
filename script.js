09030b1c0028c39a9b700246d4bd1c8287744f2f
bmokotoff7 <116100468+bmokotoff7@users.noreply.github.com>
2023-06-08 15:09:29 -0400

    Winner highlight feature working

--- script.js
+++ script.js
@@ -2,33 +2,61 @@ let homeScoreEl = document.getElementById("home-score")
 let homeScore = 0
 let guestScoreEl = document.getElementById("guest-score")
 let guestScore = 0
+let homeScoreboard = document.getElementById("home-scoreboard")
+let guestScoreboard = document.getElementById("guest-scoreboard")
 
 function add1Home() {
     homeScore += 1
     homeScoreEl.textContent = homeScore
+    highlightWinner()
 }
 
 function add2Home() {
     homeScore += 2
     homeScoreEl.textContent = homeScore
+    highlightWinner()
 }
 
 function add3Home() {
     homeScore += 3
     homeScoreEl.textContent = homeScore
+    highlightWinner()
 }
 
 function add1Guest() {
     guestScore += 1
     guestScoreEl.textContent = guestScore
+    highlightWinner()
 }
 
 function add2Guest() {
     guestScore += 2
     guestScoreEl.textContent = guestScore
+    highlightWinner()
 }
 
 function add3Guest() {
     guestScore += 3
     guestScoreEl.textContent = guestScore
+    highlightWinner()
+}
+
+function newGame() {
+
+}
+
+function highlightWinner() {
+    if (homeScore == guestScore) {
+        homeScoreboard.style.border = "5px solid white"
+        guestScoreboard.style.border = "5px solid white"
+    }
+    else if (homeScore > guestScore) {
+        homeScoreboard.style.border = "5px solid lightgreen"
+        guestScoreboard.style.border = "5px solid white"
+    }
+
+    else if (homeScore < guestScore) {
+        homeScoreboard.style.border = "5px solid white"
+        guestScoreboard.style.border = "5px solid lightgreen"
+    }
 }
